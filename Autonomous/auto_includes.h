1274afe4d6c1a07ab84d9ac24fab5cfe2cbfdfe4
DroppingDonuts <raarielgrace@gmail.com>
2014-11-15 17:16:47 -0800

    Shifted auto files in the main folder to the auto folder.

--- Autonomous/auto_includes.h
+++ Autonomous/auto_includes.h
@@ -2,23 +2,23 @@
 #define FTC_AUTO_INCLUDES
 
 ///// Sensor Multiplexer Interface /////
-#include "../drivers/hitechnic-sensormux.h"
-//For a sensor put into port 1
+#include "drivers/hitechnic-sensormux.h"
+const tMUXSensor sonarSensor = msensor_S2_1;
 const tMUXSensor IRSeeker = msensor_S2_2;
-const tMUXSensor ksTouch = msensor_S2_3;
-const tMUXSensor lTouch = msensor_S2_4;
+const tMUXSensor lightRight = msensor_S2_3;
+const tMUXSensor lightLeft = msensor_S2_4;
 
 // Other Includes //
+#include "../Sensors/light.c"
 #include "../Sensors/ir.c"
-#include "../Sensors/touch.c"
+#include "../Sensors/sonar.c"
 #include "../Sensors/gyro.c"
 #include "../Motors/motors.c"
 #include "../Motors/servos.c"
+#include "../Drive/lines.c"
 #include "../Drive/drive.c"
-//#include "../Drive/drive_auto.c" Needs to be written
-#include "JoystickDriver.c"
 
-//#include "auto_init.c" Needs to be written
-//#include "auto_functions.c" Needs to be written
+#include "auto_functions.c"
+#include ".../init_robot.c";
 
 #endif
